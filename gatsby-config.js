087ed0e0e5dd3d81a17dcbc7a3aed1481971d7e9
Vasile Boris <boris@espressoprogrammer.com>
2021-08-05 08:44:34 +0300

    Add and configure google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,6 +131,20 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-68339440-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
