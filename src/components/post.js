051e114bd1767168b23ec3ee20e317d956c4d84d
Vasile Boris <boris@espressoprogrammer.com>
2021-08-10 08:28:54 +0300

    Refactor index.js

--- src/components/post.js
+++ src/components/post.js
@@ -17,8 +17,6 @@ const Post = ({ data }) => {
         dangerouslySetInnerHTML={{ __html: post.html }}
         itemProp="articleBody"
       />
-      <footer>
-      </footer>
     </article>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,38 +23,36 @@ const Index = ({ data }) => {
   return (
     <Layout data={data}>
       <Seo title="All posts" />
-      <ol className="posts-list">
+      <div className="posts-list">
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
 
           return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
+            <article key={post.fields.slug}
+              className="post-list-item"
+              itemScope
+              itemType="http://schema.org/Article"
+            >
+              <header>
+                <h1>
+                  <Link to={post.fields.slug} itemProp="url">
+                    <span itemProp="headline">{title}</span>
+                  </Link>
+                </h1>
+                <time className="post-time">{post.frontmatter.date}</time>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: post.frontmatter.description || post.excerpt,
+                  }}
+                  itemProp="description"
+                />
+              </section>
+            </article>
           )
         })}
-      </ol>
+      </div>
     </Layout>
   )
 }
@@ -79,7 +77,7 @@ export const pageQuery = graphql`
           slug
         }
         frontmatter {
-          date(formatString: "MMMM DD, YYYY")
+          date(formatString: "D MMMM YYYY")
           title
           description
         }
